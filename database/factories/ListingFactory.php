44227232d36902b43c066ce597d601370f9e5896
AlAtsal <alexandros_atsalakis@outlook.com>
2022-07-24 23:01:33 +0300

    Create the Listing, ListingType models and their seeders, factories and migrations

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ListingType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $availability = array_rand(array_flip(Array("Sale", "Rent")),1);
+        $square_meters = rand(40,500);
+        if ($availability == "Rent"){
+            $price = mt_rand(4,6)*$square_meters;
+        } else {
+            $price = mt_rand(400, 3000)*$square_meters;
+        }
+            
+
+        
+        return [
+            'availability' => $availability,
+            'location' => array_rand(array_flip(Array("Athens", "Patra", "Thessaloniki")),1),
+            'square_meters' => $square_meters,
+            'price' => $price,            
+        ];
+    }
+}
