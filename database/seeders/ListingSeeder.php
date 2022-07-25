278e391541ea48d89a187e7342eed8ba196dbfc1
AlAtsal <alexandros_atsalakis@outlook.com>
2022-07-25 05:11:41 +0300

    Fix random array to return values (1,2,3)

--- database/seeders/ListingSeeder.php
+++ database/seeders/ListingSeeder.php
@@ -17,7 +17,7 @@ class ListingSeeder extends Seeder
     public function run()
     {
         for ($x = 0; $x <= 20; $x++) {
-            $array = array_rand([1,2,3],rand(1,3));
+            $array = array_rand(array_flip([1,2,3]),rand(1,3));
             Listing::factory()
             ->hasAttached(
                 ListingType::find($array)
